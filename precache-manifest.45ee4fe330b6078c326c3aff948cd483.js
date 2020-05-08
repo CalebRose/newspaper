9bd74806037938bc9060369357a77046c5c344a7
CalebRose <ctrose17@gmail.com>
2020-05-08 07:50:21 -0500

    Updates

---
+++ precache-manifest.45ee4fe330b6078c326c3aff948cd483.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e24c0f98f51bfd2175dbe280a0b9a4",
+    "url": "/newspaper/index.html"
+  },
+  {
+    "revision": "96bfd306ac357d13e7e9",
+    "url": "/newspaper/static/css/main.5a653c8f.chunk.css"
+  },
+  {
+    "revision": "7f8cb37a8cf22570f0c8",
+    "url": "/newspaper/static/js/2.bd410558.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/newspaper/static/js/2.bd410558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bfd306ac357d13e7e9",
+    "url": "/newspaper/static/js/main.bf63222f.chunk.js"
+  },
+  {
+    "revision": "f2605bfe0cad98fc109c",
+    "url": "/newspaper/static/js/runtime-main.24c692e5.js"
+  },
+  {
+    "revision": "8c6847c75ae35d0ca5fd3798d4567443",
+    "url": "/newspaper/static/media/AbrilFatface-Regular.8c6847c7.ttf"
+  },
+  {
+    "revision": "e9e52167470d64e2ef47dac169607ecf",
+    "url": "/newspaper/static/media/DSFetteKanzlei.e9e52167.ttf"
+  },
+  {
+    "revision": "4df62a124fcf4be7c320025403cc0567",
+    "url": "/newspaper/static/media/DSFetteKanzleiOsF.4df62a12.ttf"
+  },
+  {
+    "revision": "3fb4834bb5021fd9ffcfb278bc0a4914",
+    "url": "/newspaper/static/media/DSWalbaumfraktur.3fb4834b.ttf"
+  },
+  {
+    "revision": "7fc22409b3de5d1ba2c9eab5f3f24dc3",
+    "url": "/newspaper/static/media/ToskanischeEgyptienneInitialen.7fc22409.ttf"
+  },
+  {
+    "revision": "ebb1fc41653eec297e0e4a4ea30c1ba8",
+    "url": "/newspaper/static/media/fraktmod.ebb1fc41.ttf"
+  }
+]);
